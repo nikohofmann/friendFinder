b361a18aea6829bcdb3801ef7c0a7e33159c85bb
nikohofmann <41592016+nikohofmann@users.noreply.github.com>
2018-11-26 20:15:29 -0700

    Final working app

---
+++ README.md
@@ -0,0 +1,2 @@
+# friendFinder
+ Friend Finder is an express app that matches users based on their answers to survey questions
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,31 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+  app.post("/api/friends", function(req, res) {
+    var userInput = req.body;
+    var userScores = userInput.scores;
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 50;
+
+    for (var i = 0; i < friends.length; i++) {
+      var diff = 0;
+      
+      for (var j = 0; j < userScores.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userScores[j])
+      }
+
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
+
+    friends.push(userInput);
+
+    res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+
+  });
 }
